f8ac8f32027975f97c71fb08d157fde0a58325f8
GelP1987 <101074657+GelP1987@users.noreply.github.com>
2023-04-06 19:18:47 -0400

    added routes and started API

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+// fetch api data from TMDB
+async function fetchAPIData(endpoint) {
+  const API_KEY = "";
+}
+
+// highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll(".nav-link");
+  links.forEach((link) => {
+    if (link.getAttribute("href") === global.currentPage) {
+      link.classList.add("active");
+    }
+  });
+}
+
+// init app
+function init() {
+  switch (global.currentPage) {
+    case "/":
+    case "/index.html":
+      console.log("Home");
+      break;
+    case "/shows.html":
+      console.log("Shows");
+      break;
+    case "/movie-details.html":
+      console.log("Movie Details");
+      break;
+    case "/tv-details.html":
+      console.log("TV Details");
+      break;
+    case "/search.html":
+      console.log("Search");
+      break;
+  }
+  highlightActiveLink();
+}
+document.addEventListener("DOMContentLoaded", init);
